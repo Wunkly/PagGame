f271fb87ce158c764496df4a761118a3cf0fe1d6
Wunkly <wunkly@gmail.com>
2015-02-22 18:22:15 +0200

    Update and rename SFML-1.6/include/SFML to SFML-1.6/include/SFML/Graphics.hpp

---
+++ SFML-1.6/include/SFML/Graphics.hpp
@@ -0,0 +1,21 @@
+#ifndef SFML_GRAPHICS_HPP
+#define SFML_GRAPHICS_HPP
+
+////////////////////////////////////////////////////////////
+// Headers
+////////////////////////////////////////////////////////////
+
+#include <SFML/Window.hpp>
+#include <SFML/Graphics/Color.hpp>
+#include <SFML/Graphics/Font.hpp>
+#include <SFML/Graphics/Glyph.hpp>
+#include <SFML/Graphics/Image.hpp>
+#include <SFML/Graphics/PostFX.hpp>
+#include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/Graphics/Shape.hpp>
+#include <SFML/Graphics/Sprite.hpp>
+#include <SFML/Graphics/String.hpp>
+#include <SFML/Graphics/View.hpp>
+
+
+#endif // SFML_GRAPHICS_HPP
