ad4ac9646d7a43c803838b94c0ad5927bfab4714
Wunkly <wunkly@gmail.com>
2015-02-22 18:05:53 +0200

    Create VisibleGameObject.h

---
+++ VisibleGameObject.h
@@ -0,0 +1,29 @@
+#pragma once
+
+class VisibleGameObject
+{
+public:
+	VisibleGameObject();
+	virtual ~VisibleGameObject();
+	
+	virtual void Load(std::string filename);
+	virtual void Draw(sf::RenderWindow & window);
+	virtual void Update(float elapsedTime);
+
+	virtual void SetPosition(float x, float y);
+	virtual sf::Vector2f GetPosition() const;
+	virtual float GetWidth() const;
+	virtual float GetHeight() const;
+
+	virtual bool IsLoaded() const;
+	virtual sf::Rect<float> GetBoundingRect() const;
+
+protected:
+	sf::Sprite& GetSprite();
+
+private:
+	sf::Sprite  _sprite;
+	sf::Image _image;
+	std::string _filename;
+	bool _isLoaded;
+};
