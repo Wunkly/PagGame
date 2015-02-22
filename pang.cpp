aae5e2a69d027f800f9afd2ed729d5e913c89920
Wunkly <wunkly@gmail.com>
2015-02-22 17:50:06 +0200

    Create pang.cpp

---
+++ pang.cpp
@@ -0,0 +1,14 @@
+// pang.cpp : Defines the entry point for the console application.
+//
+
+
+#include "stdafx.h"
+#include "Game.h"
+
+int main(int argc, char** argv)
+{
+	Game::Start();
+	
+	return 0;
+}
+
